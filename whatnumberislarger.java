5a68ee5edf8bef724a17a556f9ca33ba85713c62
Yuvi2007 <144378404+Yuvi2007@users.noreply.github.com>
2023-10-15 23:19:42 -0400

    Update whatnumberislarger.java

--- whatnumberislarger.java
+++ whatnumberislarger.java
@@ -5,18 +5,18 @@ class Main {
 
 Scanner value = new Scanner(System.in); //declare scanner
 
-System.out.println("Enter Number 1: "); //ask the user for the first number
+System.out.println("Enter Number 1: "); //ask user for the 1 number
 
-int num1 = value.nextInt(); //take input from the user
+int num1 = value.nextInt(); //take input from user
 
-System.out.println("Enter Number 2"); //ask user for 2nd number
+System.out.println("Enter Number 2"); //ask user for 2 number
 
-int num2 = value.nextInt(); //take input from the user
+int num2 = value.nextInt(); //take input from user
 
 if (num1 > num2) {
-  System.out.println(num1 + " is greater than " + num2); //prints this if the first number is greater than the second
+  System.out.println(num1 + " is greater than " + num2); //prints this if the 1 number is greater than the 2
 } else if (num1 < num2) {
-  System.out.println(num1 + " is less than " + num2); //prints this statement if the 2nd number is greater then the first
+  System.out.println(num1 + " is less than " + num2); //prints this statement if the 2 number is greater then the 1
 } else {
   System.out.println(num1 + " is equal to " + num2); //prints this if both the numbers share the same value
 }
